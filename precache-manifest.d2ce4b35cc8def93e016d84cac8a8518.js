bc69348165a2cbb928b9faf74bebdc36f9db4399
alexasteria <replay.faq@gmail.com>
2020-09-12 01:16:32 +0300

    Updates

---
+++ precache-manifest.d2ce4b35cc8def93e016d84cac8a8518.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c92c0866e0972b4e49b557b7dd95024e",
+    "url": "/memes/index.html"
+  },
+  {
+    "revision": "e72dec4bb53c3adcd063",
+    "url": "/memes/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "e72dec4bb53c3adcd063",
+    "url": "/memes/static/js/2.04e9903a.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/memes/static/js/2.04e9903a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9914852595e06a0c15d7",
+    "url": "/memes/static/js/main.53c7c3b7.chunk.js"
+  },
+  {
+    "revision": "8fe99b4cc464277b9b93",
+    "url": "/memes/static/js/runtime-main.a7980ffa.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/memes/precache-manifest.35d86d1418704fbe42b904851761b3f9.js"
+  "/memes/precache-manifest.d2ce4b35cc8def93e016d84cac8a8518.js"
 );
 
 self.addEventListener('message', (event) => {
